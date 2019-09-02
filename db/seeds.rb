dc2c6d4dd2c3066195e04678bac0db194d68f1ac
Roger Kiew <yuichiu416@gmail.com>
2019-09-02 16:21:05 -0500

    added 3 stocks to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+s1 = Stock.create(name: "Vanguard Total Stock Market ETF", symbol: "VTI", price: 148.96, today:-0.05, popularity: 28715, analyst_ratings: 0, in_watchlist: true)
+s2 = Stock.create(name: "AMD", symbol: "AMD", price: 31.38, today:0.0, popularity: 174205, analyst_ratings: 33, in_watchlist: true)
+s3 = Stock.create(name: "Microsoft", symbol: "MSFT", price: 137.74, today:-0.26, popularity: 218318, analyst_ratings: 91)
