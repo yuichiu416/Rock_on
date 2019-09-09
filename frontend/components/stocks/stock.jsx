e0d1eb90c2e6ce0f3aef375fadbff60adb0fa9b8
Roger Kiew <yuichiu416@gmail.com>
2019-09-09 10:35:21 -0500

    fixed the state issue and the navbar positioning issue. Can work on the actual content now

--- frontend/components/stocks/stock.jsx
+++ frontend/components/stocks/stock.jsx
@@ -3,11 +3,10 @@ import React, { Component } from 'react'
 class Stock extends Component {
     constructor(props){
         super(props)
-        this.props.fetchStock("MSFT").then(stock => console.log(stock));
-        //question1. how to get the stock information from the response?
-        //question2. the dive.flex doesn't like any children 
     }
-
+    componentDidMount(){
+        this.props.fetchStock("MSFT").then(stocks => this.setState(stocks));
+    }
     render() {
         return (
             <div id="stock">
--- frontend/components/stocks/stock_container.jsx
+++ frontend/components/stocks/stock_container.jsx
@@ -1,12 +1,10 @@
 import { connect } from 'react-redux';
 import { fetchStock } from '../../actions/stock_actions';
 import { logout } from '../../actions/session_actions';
-
 import Stock from './stock';
 
-
 const masStateToProps = state => ({
-
+    stocks: state.entities.stocks
 });
 const mapDispatchToProps = dispatch => ({
     fetchStock: ticker => dispatch(fetchStock(ticker)),
