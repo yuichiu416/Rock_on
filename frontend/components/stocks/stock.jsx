3aa4c8ce65ef2caf01f56a6c5408c9e97615caba
Roger Kiew <yuichiu416@gmail.com>
2019-09-09 17:52:41 -0500

    can fetch and display stocks properly, need to work on the display panel css

--- frontend/components/stocks/stock.jsx
+++ frontend/components/stocks/stock.jsx
@@ -1,16 +1,41 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Watchlist from './watchlist';
 
 class Stock extends Component {
     constructor(props){
-        super(props)
+        super(props);
+        this.handleFetch = this.handleFetch.bind(this);
     }
-    componentDidMount(){
-        this.props.fetchStock("MSFT").then(stocks => this.setState(stocks));
+    handleFetch(ticker){
+        this.props.fetchStock(ticker).then(stocks => this.setState(stocks));
     }
     render() {
+        let container, ul;
+        if(this.state && this.state.stock)
+            container = this.state;
+        if(container && container.stock){
+            ul = 
+            <ul>
+                <li>Symbol: {container.stock.symbol}</li>
+                <li>Exchange: {container.stock.exchange}</li>
+                <li>Induxtry: {container.stock.induxtry}</li>
+                <li>CEO: {container.stock.CEO}</li>
+                <li>Address: {container.stock.address}</li>
+                <li>City: {container.stock.city}</li>
+                <li>Company Name: {container.stock.companyName}</li>
+                <li>Country: {container.stock.country}</li>
+                <li><p>Description: {container.stock.description}</p></li>
+                <li>Security Name: {container.stock.securityName}</li>
+                <li>Tags: {container.stock.tags}</li>
+                <li>Website: {container.stock.website}</li>
+            </ul>
+        }
+        else 
+            ul = <div id="loading-text">Loading Stock Information</div>
         return (
             <div id="stock">
-                Stock goes here
+                {ul}
+                <Watchlist handleFetch={this.handleFetch} />
             </div>
         )
     }
