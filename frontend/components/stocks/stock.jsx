b339e2d482a6799aa72e84c08aaf9de1877967a6
Roger Kiew <yuichiu416@gmail.com>
2019-09-09 00:40:44 -0500

    can fetch stock and get the right state, needs to actually render the information. Need to solve the nested flexbox issue

--- frontend/components/stocks/stock.jsx
+++ frontend/components/stocks/stock.jsx
@@ -3,9 +3,12 @@ import React, { Component } from 'react'
 class Stock extends Component {
     constructor(props){
         super(props)
+        this.props.fetchStock("MSFT").then(stock => console.log(stock));
+        //question1. how to get the stock information from the response?
+        //question2. the dive.flex doesn't like any children 
     }
+
     render() {
-        this.props.fetchStock("MSFT");
         return (
             <div id="stock">
                 Stock goes here
--- frontend/components/stocks/stock_container.jsx
+++ frontend/components/stocks/stock_container.jsx
@@ -1,5 +1,7 @@
 import { connect } from 'react-redux';
 import { fetchStock } from '../../actions/stock_actions';
+import { logout } from '../../actions/session_actions';
+
 import Stock from './stock';
 
 
@@ -8,6 +10,7 @@ const masStateToProps = state => ({
 });
 const mapDispatchToProps = dispatch => ({
     fetchStock: ticker => dispatch(fetchStock(ticker)),
+    logout: () => dispatch(logout()),
 });
 
 export default connect(masStateToProps, mapDispatchToProps)(Stock);
