b475a1708c072a7b3c6a86c493d68e358e1c4969
Roger Kiew <yuichiu416@gmail.com>
2019-11-08 21:59:06 -0600

    changed watchlist from table to ul/li, gotta do css

--- frontend/components/stocks/stock_index.jsx
+++ frontend/components/stocks/stock_index.jsx
@@ -6,7 +6,6 @@ class Stock extends Component {
     constructor(props) {
         super(props);
         this.state = {total: 0, stocks: [], balance: 0};
-        this.handlePortfolio = this.handlePortfolio.bind(this);
     }
     
     calculateBalance(deposits) {
@@ -17,49 +16,25 @@ class Stock extends Component {
     componentDidMount(){
         const user_id = this.props.currentUser.id;
         this.props.fetchTransactions(user_id).then(() => this.calculateBalance(this.props.deposits.deposit));
-        this.props.getAllStocksHaving(user_id).then(() => this.handlePortfolio());
     }
-    handlePortfolio(){
-        const portfolio = this.props.portfolio;
-        for(const ticker in portfolio){
-            const balance = 0, shares = 0;
-            portfolio[ticker].map(info => {
-                balance += info.price * info.num_shares;
-                shares += info.num_shares;
-            });
-            this.setState({total: this.state.total + balance, stocks: this.state.stocks.concat({shares: shares, ticker: ticker})});
-        }
-    }
+
     render() {
-        const list = this.state.stocks.map(info => {
-            if(info.shares === 0)
-                return <li>No stocks in hand</li>;
-            return <li key={info.ticker}><Link to={`/stocks/${info.ticker}`}>{info.ticker}</Link>: {info.shares} shares</li>
-        });
         return (
             <div className="stock">
                 <div className="portfolio">
                     <h1>Portfolio</h1><br/><br/>
-                    <table className="portfolio-table">
-                        <tbody>
-                            <tr>
-                                <td><h3>Total stock value:</h3></td>
-                                <td><h3 className="margin-auto"> ${this.state.total.toFixed(2)}</h3></td>
-                            </tr>
-                            <tr>
-                                <td><h3>Cash balance:</h3></td>
-                                <td><h3 className="margin-auto"> ${this.state.balance.toFixed(2)}</h3></td>
-                            </tr>
-                            <tr>
-                                <td><h3>Total Account value:</h3></td>
-                                <td><h3 className="margin-auto"> ${(this.state.total + this.state.balance).toFixed(2)}</h3></td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    
-                    <h3>Stocks in hand:</h3>
-                    <ul className="white-sheet">
-                        {list}
+                    <ul className="portfolio-table">
+                        <ul>
+                            <li><h3>Total stock value:</h3></li>
+                            <li><h3 className="margin-auto"> ${this.state.total.toFixed(2)}</h3></li>
+                
+                            <li><h3>Cash balance:</h3></li>
+                            <li><h3 className="margin-auto"> ${this.state.balance.toFixed(2)}</h3></li>
+                
+                            <li><h3>Total Account value:</h3></li>
+                            <li><h3 className="margin-auto"> ${(this.state.total + this.state.balance).toFixed(2)}</h3></li>
+                           
+                        </ul>
                     </ul>
                 </div>
                 <Watchlist/>
--- frontend/components/stocks/watchlist.jsx
+++ frontend/components/stocks/watchlist.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import Table from '@material-ui/core/Table';
 import Paper from '@material-ui/core/Paper';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { fetchTransactions, getAllStocksHaving } from "../../actions/stock_actions";
 
 export const stockList = [
     "MSFT",
@@ -42,29 +43,67 @@ export const stockList = [
 ];
 
 class Watchlist extends Component{
-    render(){
-        const trs = stockList.map((ticker, idx) => {
-            return (
-                <tr key={ticker}>
-                    <td><Link to={`/stocks/${ticker}`}>{idx}</Link></td>
-                    <td><Link to={`/stocks/${ticker}`}>{ticker}</Link></td>
-                </tr>
-            );
+    constructor(props){
+        super(props);
+        this.handlePortfolio = this.handlePortfolio.bind(this);
+        this.state = {stocks: [] };
+    }
+
+    componentDidMount(){
+      const user_id = this.props.currentUser.id;
+      this.props.getAllStocksHaving(user_id).then(() => this.handlePortfolio());
+    }
+
+    handlePortfolio() {
+      const portfolio = this.props.portfolio;
+      for (const ticker in portfolio) {
+        const balance = 0, shares = 0;
+        portfolio[ticker].map(info => {
+          balance += info.price * info.num_shares;
+          shares += info.num_shares;
         });
+  
+        this.setState({ total: this.state.total + balance, stocks: this.state.stocks.concat({ shares: shares, ticker: ticker }) });
+      }
+    }
+
+    render(){
+        const shares = this.state.stocks.map(info => {
+          if (info.shares === 0)
+            return <li></li>;
+          return <li key={info.ticker} ><Link to={`/stocks/${info.ticker}`}>{info.ticker}<br />{info.shares} shares</Link></li>
+
+        });
+        // const list = stockList.map((ticker, idx) => {
+        //     return (
+        //         <ul key={ticker}>
+        //             <li><Link to={`/stocks/${ticker}`}>{idx}</Link></li>
+        //             <li><Link to={`/stocks/${ticker}`}>{ticker}</Link></li>
+        //         </ul>
+        //     );
+        // });
         return (
           <Paper className="watchlist white-sheet">
-            <Table>
-              <thead>
-                <tr className="watchlist-header">
-                  <th>#</th>
-                  <th>Stock Symbol</th>
-                </tr>
-              </thead>
-              <tbody>{trs}</tbody>
-            </Table>
+                <div className="watchlist-header">
+                  <span>Stock Symbol</span><br/>
+                  {shares}
+                </div>
+              {/* <div>{list}</div> */}
           </Paper>
         );
     }
 };
 
-export default Watchlist;
+const mapStateToProps = (state) => {
+  return {
+    currentUser: state.session.currentUser,
+    portfolio: state.entities.transactions.portfolio
+
+  };
+};
+
+const mapDispatchToProps = dispatch => ({
+  getAllStocksHaving: user_id => dispatch(getAllStocksHaving(user_id))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Watchlist);
