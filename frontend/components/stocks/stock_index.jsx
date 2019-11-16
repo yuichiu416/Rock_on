6b71b2e6764187a5d571abc0f70622a0694300e4
Roger Kiew <yuichiu416@gmail.com>
2019-11-16 16:14:34 -0600

    changed the graph algorithm

--- frontend/components/stocks/stock_index.jsx
+++ frontend/components/stocks/stock_index.jsx
@@ -11,35 +11,30 @@ class Stock extends Component {
             stocks: [], 
             balance: 0,
             "1D": [],
-            "5dm": [],
-            "1mm": [],
+            "5D": [],
+            "1M": [],
             "3M": [],
             "1Y": [],
             "5Y": [],
-            fetched: false,
             timeFrame: "1D",
-            tickerSymbol: "",
-            openValue: null,
-            closeValue: null,
-            change: 0,
-            changePercent: 0
         }
         this.generateData = this.generateData.bind(this);
         this.handlePortfolio = this.handlePortfolio.bind(this);
         this.updatePrices = this.updatePrices.bind(this);
-        this.dailyVal = this.dailyVal.bind(this);
+        this.calculateBalance = this.calculateBalance.bind(this);
     }
     
-    calculateBalance(deposits) {
-        if (!deposits || deposits.length < 1)
-        return;
-        this.setState({ balance: deposits.reduce((a, b) => a + b) });
-    }
     componentDidMount(){
         const user_id = this.props.currentUser.id;
         this.props.fetchTransactions(user_id).then(() => this.calculateBalance(this.props.deposits.deposit));
         this.props.getAllStocksHaving(user_id).then(() => this.handlePortfolio());
         this.generateData();
+    }
+
+    calculateBalance(deposits) {
+        if (!deposits || deposits.length < 1)
+        return;
+        this.setState({ balance: deposits.reduce((a, b) => a + b) });
     }
 
     handlePortfolio() {
@@ -57,109 +52,82 @@ class Stock extends Component {
     generateData() {
         let data = [];
         var current = new Date();
+        this.current = current;
         var yesterday = new Date(current - 86400000);
         var value = 100000;
         var yesterdayVal = this.valueChange(value);
 
-        while (data.length < 1900) {
+        while (data.length < 3650) {
             data.push({ date: yesterday, value: this.valueChange(value) });
             current = yesterday;
             yesterdayVal = value;
             value = this.valueChange(yesterdayVal);
             yesterday = new Date(current - 86400000);
         }
+        this.data = data;
         this.setState({ data: data });
     }
 
     valueChange(val) {
-        let decrease = Math.floor(Math.random() * 10) % 2 === 0 && Math.floor(Math.random() * 10) % 3 === 0;
-        let rate = 1;
-        if (decrease)
-            rate *= -1;
-        return (1 + rate * Math.random() / 100) * val;
+        let random = Math.random() * 0.1;
+        return (1.05 - random) * val;
     }
 
     updatePrices(timeFrame) { // CLICKED TIMEFRAME CALC
-        if (this.state.timeFrame !== timeFrame && timeFrame !== '1D') {
-
-            this.weeklyPrices = {}
-            let that = this;
-            Object.values(this.props.transactions).forEach((asset, idx) => {
-
-                if (this.props.tickers[asset.ticker_symbol.toUpperCase()]) {
-                    const createdAt = new Date(Date.parse(`${asset.created_at}`))//.toLocaleString('en-US')
-
-                    fetchPrices(asset.ticker_symbol, timeFrame).then(prices => {
-                        let num_shares = asset.purchase_shares
-                        prices.forEach(close_price => {
-                            const date = close_price.minute ? new Date(Date.parse(`${close_price.date} ${close_price.minute}`)) : new Date(Date.parse(`${close_price.date}`))//.toLocaleString('en-US')
-
-                            if (date > createdAt && close_price.close !== null) {
-                                if (that.weeklyPrices[date.toLocaleString('en-US')] >= 0) {
-                                    that.weeklyPrices[date.toLocaleString('en-US')] += close_price.close * num_shares
-                                } else {
-                                    that.weeklyPrices[date.toLocaleString('en-US')] = (close_price.close * num_shares) + parseFloat(this.props.currentBuyingPower)
-                                }
-                            }
-                        })
-
-
-                        if (idx === this.props.transactions.length - 1) {
-                            const newArr = Object.keys(that.weeklyPrices).map(key => {
-                                return { "date": key, "value": that.weeklyPrices[key] }
-                            })
-
-                            that.setState({ fetched: true, portfolioValue: newArr })
-
-                        }
-                    }).then(this.setState({ timeFrame: timeFrame }))
-                }
-            })
-
-        } else if (timeFrame === '1D') {
-            this.props.fetchTransactions().then(response => {
-                this.dailyVal(response)
-            }).then(this.setState({ timeFrame: timeFrame }))
+        var data = [];
+        var current = this.current;
+        var last;
+        if (timeFrame === '1D') {
+            for(let i = 0; i < 180; i++){
+                let datum = this.data[i];
+                last = new Date(current - 4800);
+                data.push({ date: last, value: datum.value });
+                current = last;
+            }
+            this.setState({ data: data });
+        } else if(timeFrame === "5D"){
+            for (let i = 200; i < 600; i++) {
+                let datum = this.data[i];
+                last = new Date(current - 24000);
+                data.push({ date: last, value: datum.value });
+                current = last;
+            }
+            this.setState({ data: data });
+        } else if(timeFrame === "1M"){
+            for (let i = 550; i < 1280; i++) {
+                let datum = this.data[i];
+                last = new Date(current - 144000);
+                data.push({ date: last, value: datum.value });
+                current = last;
+            }
+            this.setState({ data: data });
+        } else if (timeFrame === "3M") {
+            for (let i = 280; i < 880; i++) {
+                let datum = this.data[i];
+                last = new Date(current - 432000);
+                data.push({ date: last, value: datum.value });
+                current = last;
+            }
+            this.setState({ data: data });
+        } else if(timeFrame === "1Y") {
+            for (let i = 0; i < 1820; i++) {
+                let datum = this.data[i];
+                last = new Date(current - 1728000);
+                data.push({ date: last, value: datum.value });
+                current = last;
+            }
+            this.setState({ data: data });
+        } else{
+            this.setState({ data: this.data })
         }
-        // this.setState({timeFrame: timeFrame})
     }
 
-    dailyVal(response) { // DAILY PORTFOLIO CALC
-        let that = this;
-
-        const data = response.transactions.forEach((asset, idx) => {
-            if (this.props.tickers[asset.ticker_symbol.toUpperCase()]) {
-                const createdAt = new Date(Date.parse(`${asset.created_at}`))//.toLocaleString('en-US')
-
-                fetchDailyPrices(asset.ticker_symbol).then(prices => {
-                    let num_shares = asset.purchase_shares
-                    prices.forEach(close_price => {
-                        const date = new Date(Date.parse(`${close_price.date} ${close_price.minute}`))//.toLocaleString('en-US')
-
-                        if (date > createdAt && close_price.close !== null) {
-                            if (that.dailyPrices[date.toLocaleString('en-US')] >= 0) {
-                                that.dailyPrices[date.toLocaleString('en-US')] += close_price.close * num_shares
-                            } else {
-                                that.dailyPrices[date.toLocaleString('en-US')] = (close_price.close * num_shares) + parseFloat(this.props.currentBuyingPower)
-                            }
-                        }
-                    })
-
-                    if (idx === response.transactions.length - 1) {
-                        const newArr = Object.keys(that.dailyPrices).map(key => {
-                            return { "date": key, "value": that.dailyPrices[key] }
-                        })
-                        that.setState({ fetched: true, portfolioValue: newArr })
-
-                    }
-
-                })
-            }
-        })
+    dailyVal() {
+        this.setState({ data: this.data});
     }
     render() {
         const tF = Object.keys(this.state).map(key => {
-            if (key === "1D" || key === "5dm" || key === "1mm" || key === "3M" || key === "1Y" || key === "ALL") {
+            if (key === "1D" || key === "5D" || key === "1M" || key === "3M" || key === "1Y" || key === "5Y") {
                 return <button className="btns" key={`${key}-id`} onClick={() => { this.updatePrices(key) }}>
                     {key.slice(0, 2).toUpperCase()}
                 </button>
